bff3df8af4c4681cc9f1d522a8a6b27f5e1fd6a3
Carlos Rosales <carlosjrr@hotmail.com>
2019-05-15 08:42:30 -0300

    cambios en bootstrap

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -12,7 +12,24 @@ namespace MvcMovie.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+
+            List<string> images = new List<string>();
+            images.Add("https://static.flickr.com/66/199481236_dc98b5abb3_s.jpg");
+            images.Add("https://static.flickr.com/75/199481072_b4a0d09597_s.jpg");
+            images.Add("https://static.flickr.com/57/199481087_33ae73a8de_s.jpg");
+            images.Add("https://static.flickr.com/77/199481108_4359e6b971_s.jpg");
+            images.Add("https://static.flickr.com/58/199481143_3c148d9dd3_s.jpg");
+            images.Add("https://static.flickr.com/72/199481203_ad4cdcf109_s.jpg");
+            images.Add("https://static.flickr.com/58/199481218_264ce20da0_s.jpg");
+            images.Add("https://static.flickr.com/69/199481255_fdfe885f87_s.jpg");
+            images.Add("https://static.flickr.com/60/199480111_87d4cb3e38_s.jpg");
+            images.Add("https://static.flickr.com/70/229228324_08223b70fa_s.jpg");
+
+            return View(images);
+
+
+
+            //return View();
         }
 
         public IActionResult Privacy()
