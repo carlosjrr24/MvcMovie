c538068d419d5d95af415d13a9a3aa52f8cb7c2b
Carlos Rosales <carlosjrr@hotmail.com>
2019-05-17 16:21:21 -0300

    varios cambios referentes al formato de decimales

---
+++ MvcMovie/Migrations/20190516184012_03.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class _03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
-        public int Id { get; set; }
+        public int Id    { get; set; }
+
+        [Required]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        //[DisplayFormat(DataFormatString = "{0:n0}")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
